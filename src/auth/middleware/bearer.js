1851ac6702981256544704de0105a0e8f5027920
EvaGraceSmith <smithevagrace@gmail.com>
2023-05-25 20:48:03 -0500

    added update and delete

---
+++ src/auth/middleware/bearer.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const { users } = require('../models/index.js');
+
+module.exports = async (req, res, next) => {
+
+  try {
+    if (!req.headers.authorization) { return _authError(); }
+
+    const token = req.headers.authorization.split(' ').pop();
+    const validUser = await users.authenticateToken(token);
+    req.user = validUser;
+    req.token = validUser.token;
+    next();
+  } catch (e) {
+    console.error(e.message || e);
+    _authError();
+  }
+  function _authError() {
+    res.status(403).send('Invalid Login line 20');
+  }
+
+};
--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,9 @@
 const express = require('express');
 const router = express.Router();
 const {users} = require('../auth/models/index.js');
-// const basicAuth = require('../auth/middleware/basic.js');
-// const bearerAuth = require('../auth/middleware/bearer.js');
-// const permissions = require('../auth/middleware/acl.js');
+const basicAuth = require('../auth/middleware/basic.js');
+const bearerAuth = require('../auth/middleware/bearer.js');
+const permissions = require('../auth/middleware/acl.js');
 
 
 router.post('/signup', async (req, res, next) => {
@@ -17,11 +17,11 @@ router.post('/signup', async (req, res, next) => {
   }
 });
 
-// router.post('/signin', basicAuth, (req, res, next) => {
-//   res.status(200).json(req.user);
-// });
+router.post('/signin', basicAuth, (req, res, next) => {
+  res.status(200).json(req.user);
+});
 
-router.get('/users', async (req, res, next) => {
+router.get('/users', bearerAuth,  permissions('delete'), async (req, res, next) => {
   try {
     const allUsers = await users.findAll({});
     const list = allUsers.map(user => user.username);
@@ -31,15 +31,41 @@ router.get('/users', async (req, res, next) => {
   }
 });
 
-// router.get('/secret', basicAuth, (req, res, next) => {
-//   res.status(200).send('Welcome to the secret area!');
-// });
+router.get('/secret', basicAuth, (req, res, next) => {
+  res.status(200).send('Welcome to the secret area!');
+});
 
 
 router.get('/', (req, res) => {
   res.status(200).send('Hello World');
 },
 );
+
+router.get('/bad', (req, res, next) => {
+  next('bad request');
+},
+);
+
+router.put('/update/:id', bearerAuth, permissions('update'), async (req, res, next) => {
+  try {
+    const updatedUser = await users.update(req.body, {where: {id: req.params.id}});
+    res.status(200).json(updatedUser);
+  } catch (e) {
+    next(e.message);
+  }
+});
+
+router.delete('/delete/:id', bearerAuth, permissions('delete'), async (req, res, next) => {
+  try {
+    const deletedUser = await users.destroy({where: {id: req.params.id}});
+    res.status(200).json(deletedUser);
+  } catch (e) {
+    next(e.message);
+  }
+});
+
+
+
 
 
 module.exports = router;
